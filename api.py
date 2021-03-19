dd29cc2b0f3bc1ee560c5753b108d5e3708ab01e
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-03-20 00:18:37 +0100

    not works with multiple sensors

--- api.py
+++ api.py
@@ -10,29 +10,31 @@ import platform
 app = Flask(__name__)
 api = Api(app)
 os = platform.system()
+file_name = '/w1_slave'
+
 if os == 'Linux':
     base_dir = '/sys/bus/w1/devices/'
-    device_folder = glob.glob(base_dir + '28*')[0]
-    device_file = device_folder + '/w1_slave'
+    delimiter = '/'
 elif os == 'Windows':
     # testing on windows
-    device_file = 'w1_slave.txt'
+    base_dir = 'test/'
+    delimiter = '\\'
+
+device_list = [ f.split(delimiter)[-1] for f in glob.glob(base_dir + '28*')]
 
 class TempSensor:
-    def get_objects(self):
-        data = {
+    def serialize(self):
+        return  {
             'sensor_id': self.id
         }
-        return jsonify(data)
 
-    def __init__(self, n, i, f):
-        self.id = i
-        self.name = n
-        self.file = f
+    def __init__(self, name, id):
+        self.id = id
+        self.name = name
 
 class TempReading:
-    def get_objects(self):
-        data = {
+    def serialize(self):
+        return  {
             'temperature': self.temp,
             'sensor': {
                 'name': self.sensor.name,
@@ -40,21 +42,20 @@ class TempReading:
             },
             'date': self.date
         }
-        return jsonify(data)
 
     def __init__(self, t, s):
         self.temp = t
         self.sensor = s
         self.date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(device_id):
+    f = open(base_dir +  device_id + file_name, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
-def read_temp():
-    lines = read_temp_raw()
+def read_temp(device_id):
+    lines = read_temp_raw(device_id)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
@@ -67,20 +68,31 @@ def read_temp():
 parser = reqparse.RequestParser()
 parser.add_argument('task')
 
+def abort_if_sensor_doesnt_exist(sensor_id):
+    if sensor_id not in device_list:
+        abort(404, message="Device ID {} doesn't exist".format(sensor_id))
 
-# Todo
-# shows a single todo item and lets you delete a todo item
 class Temperature(Resource):
+    def get(self, sensor_id):
+        abort_if_sensor_doesnt_exist(sensor_id)
+        s = TempSensor('vand_ud', sensor_id)
+        r = TempReading(read_temp(sensor_id), s)
+        return r.serialize()
+
+class TemperatureList(Resource):
     def get(self):
-        s = TempSensor('vand_ud', '28-dummy', device_file)
-        r = TempReading(read_temp(), s)
-        return r.get_objects()
+        sensors = []
+        for sensor_id in device_list:
+            s = TempSensor('vand_ud', sensor_id)
+            r = TempReading(read_temp(sensor_id), s)
+            sensors.append(r)
+        return jsonify(sensorlist=[s.serialize() for s in sensors])
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(Temperature, '/temp')
-#api.add_resource(Temperature, '/temp/<sensor_id>')
+api.add_resource(TemperatureList, '/temp')
+api.add_resource(Temperature, '/temp/<sensor_id>')
 
 
 if __name__ == '__main__':
